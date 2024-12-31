a47407ab45dd6f9015586eb857ef92fa041d6caf
Mathias Kluge <59650843+mathiaskluge@users.noreply.github.com>
2024-12-31 14:38:42 +0100

    refactored tests

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const enHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return enHelloPrefix + name
 }
 
 func main() {
